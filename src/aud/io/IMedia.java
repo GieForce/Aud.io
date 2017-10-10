498c4ad9254de4f380fd0894a1e46a061d66f383
Ruud Schouten <ruud.schouten@outlook.com>
2017-10-10 10:46:53 +0200

    Removed SpotifyMedia, as we're no longer going to use it

--- src/aud/io/IMedia.java
+++ src/aud/io/IMedia.java
@@ -1,10 +1,7 @@
 package aud.io;
 
-import net.sf.json.JSONObject;
-
 import java.util.Properties;
 
 public interface IMedia {
-    JSONObject getFile(String zoekterm);
     Properties getProperties();
 }
