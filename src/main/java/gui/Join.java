a790fcbd51f272e5d12618c174ac1ffea655610a
Ruud Schouten <ruud.schouten@outlook.com>
2017-10-24 13:24:07 +0200

    Added JavaFX class for Join

---
+++ src/main/java/gui/Join.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+
+public class Join extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/join.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
