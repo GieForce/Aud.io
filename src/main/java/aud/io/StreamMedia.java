6336c8cb3cb8e46f525e3bcf1e9f6e22277bd70d
Nick Hammes <220595@student.fontys.nl>
2017-10-27 15:00:31 +0200

    Implemented Play in StreamMedia

--- src/main/java/aud/io/StreamMedia.java
+++ src/main/java/aud/io/StreamMedia.java
@@ -5,7 +5,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jongo.marshall.jackson.oid.MongoObjectId;
 
+import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.net.Socket;
 
 public class StreamMedia implements IMedia, Serializable {
 
@@ -26,8 +31,39 @@ public class StreamMedia implements IMedia, Serializable {
     public void play() {
         //TODO:Toevoegen afspelen van Streammedia
 
-        //Praat met audioserver voor ophalen media
+        (new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Praat met audioserver voor ophalen media
+                try {
+                    Socket socket = new Socket(streamLocation, 6666);
+                    if (socket.isConnected()) {
+                        InputStream in = new BufferedInputStream(socket.getInputStream());
+                        playAudio(in);
+                    }
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            private void playAudio(final InputStream in) {
+                try {
+                    AudioInputStream ais = AudioSystem.getAudioInputStream(in);
+                    Clip audioClip = AudioSystem.getClip();
+                    audioClip.open(ais);
+                    audioClip.start();
+                    Thread.sleep(100); //Clip heeft de kans om op te starten
+                    audioClip.drain();
+                } catch (IOException | InterruptedException | LineUnavailableException | UnsupportedAudioFileException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        })).start();
+
 
         //
     }
+
+
 }
