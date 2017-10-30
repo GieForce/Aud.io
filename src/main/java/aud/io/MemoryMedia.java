42af7d1fdf523f6a296ffacb9d661a359cfab5a6
Nick Hammes <220595@student.fontys.nl>
2017-10-30 12:03:59 +0100

    added static audio playing

--- src/main/java/aud/io/MemoryMedia.java
+++ src/main/java/aud/io/MemoryMedia.java
@@ -1,5 +1,9 @@
 package aud.io;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
 import java.io.Serializable;
 import java.util.Properties;
 
@@ -18,7 +22,21 @@ public class MemoryMedia implements IMedia, Serializable {
             @Override
             public void run() {
                 System.out.printf("Now playing: %s%n", name);
+                playSound();
             }
+
+            private void playSound() {
+                try {
+                    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("AudioFile/Demo.wav").getAbsoluteFile());
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioInputStream);
+                    clip.start();
+                } catch (Exception ex) {
+                    System.out.println("Error with playing sound.");
+                    ex.printStackTrace();
+                }
+            }
+
         })).start();
     }
 }
