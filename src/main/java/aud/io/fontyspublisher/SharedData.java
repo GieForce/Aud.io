841b5667331b3c691b97264cc7e7c53797688c49
Nick Hammes <220595@student.fontys.nl>
2017-12-18 11:46:52 +0100

    prepared for online testing

--- src/main/java/aud/io/fontyspublisher/SharedData.java
+++ src/main/java/aud/io/fontyspublisher/SharedData.java
@@ -2,7 +2,7 @@ package aud.io.fontyspublisher;
 
 public class SharedData {
     private static final int PORT = 1099;
-    private static final String REGISTRY_NAME = "192.168.1.1";
+    private static final String REGISTRY_NAME = "145.93.72.231";
     private static final String SERVER_NAME = "partyServer";
     private static final String PUBLISHER_NAME = "serverPublisher";
 
